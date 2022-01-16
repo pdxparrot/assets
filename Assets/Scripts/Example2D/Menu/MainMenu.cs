c37149b7565fde3829ec6f2ceebb41a61a33608c
Shane Lillie <ignignokterr@gmail.com>
2022-01-16 10:34:11 -0800

    Example main menu

---
+++ Assets/Scripts/Example2D/Menu/MainMenu.cs
@@ -0,0 +1,18 @@
+using pdxpartyparrot.Game.State;
+
+namespace pdxpartyparrot.Example2D.Menu
+{
+    public sealed class MainMenu : Game.Menu.MainMenu
+    {
+        #region Events
+
+        public override void OnStart()
+        {
+            base.OnStart();
+
+            // TODO: transition to the next step
+        }
+
+        #endregion
+    }
+}
